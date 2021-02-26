9b8c9f626dec186aa250c11102872f0d38f207d8
fish-uncle <fishuncle@126.com>
2021-02-26 22:34:00 +0800

    fix: init

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import 'view-design/dist/styles/iview.css'
 import dEditor from './src/components/d-editor'
 import widgetMixin, {configMerge} from './mixins'
 import commonConfigValue from './common-config-value'
 import platform from './src/store/platform.store'
 import './src/api'
+import 'view-design/dist/styles/iview.css'
+import './src/scss/reset.scss'
+import './src/scss/index.scss'
 
 export {
 	commonConfigValue,
