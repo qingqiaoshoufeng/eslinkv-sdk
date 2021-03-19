38f8d38cdbc62aebb3200b606c3071b8492e3068
xjm <jiaming.xu@eslink.com>
2021-03-19 15:19:50 +0800

    fix: publish

--- src/components/d-footer/publish.mx.ts
+++ src/components/d-footer/publish.mx.ts
@@ -1,25 +1,26 @@
 export default {
-	methods: {
-		publishBoard () {
-			if (this.kanboardEdited) {
-				this.$Message.warning('请先保存看板！')
-				return
-			}
-			this.$Modal.confirm({
-				title: '提示',
-				content: '确认发布此看板吗？',
-				loading: true,
-				onOk: () => {
-					this.$api.screen.publish({ id: this.$parent.kanboardId }).then(res => {
+    methods: {
+        publishBoard () {
+            if (this.kanboardEdited) {
+                this.$Message.warning('请先保存看板！')
+                return
+            }
+            this.$Modal.confirm({
+                title: '提示',
+                content: '确认发布此看板吗？',
+                loading: true,
+                onOk: () => {
+                    const { params: { id } } = this.$route
+                    this.$api.screen.publish({ screenId: id }).then(res => {
                         this.$Message.success('发布成功！')
                         this.$router.push('/')
                         this.exit()
-						this.$Modal.remove()
-					}).catch(() => {
-						this.$Modal.remove()
-					})
-				}
-			})
-		}
-	}
+                        this.$Modal.remove()
+                    }).catch(() => {
+                        this.$Modal.remove()
+                    })
+                }
+            })
+        }
+    }
 }
