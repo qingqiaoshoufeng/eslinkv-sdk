3552342cba11383e734f7635b54e9484be43daa3
zhepei.shen <zhepei.shen@eslink.com>
2021-04-26 14:28:05 +0800

    fix: 新建大屏页面，新建参考线，无法保存，且会报错
    fix #58

--- src/components/d-guide/prod.ts
+++ src/components/d-guide/prod.ts
@@ -2,10 +2,12 @@ export default {
 	methods: {
 		updateHandle() {
 			const id = this.$route.params.id
-			this.$api.screenShare.screenShareUpdate({
-				screenId: id,
-				screenGuide: this.platform.ruler.guideLines,
-			})
+			if (id) {
+				this.$api.screenShare.screenShareUpdate({
+					screenId: id,
+					screenGuide: this.platform.ruler.guideLines,
+				})
+			}
 		},
 	},
 }
