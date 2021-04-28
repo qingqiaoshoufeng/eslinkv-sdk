eb0c12a24b7b1177603a0edc22b85d7ba8b86bcc
xjm <jiaming.xu@eslink.com>
2021-04-28 11:57:44 +0800

    fix: upload 优化

--- src/components-func/func-image.vue
+++ src/components-func/func-image.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
 .d-manage-modal-control
-	label {{ config.label }}
+	label
+		span(style="margin-right: 5px;") {{ config.label }}
+		tooltip(content="支持jpg，png，gif" placement="top-start")
+			i-icon(type="ios-help-circle-outline" size="16")
 	.d-manage-modal-control-right
 		d-upload(v-model="obj[inputKey]", :data="formData")
 </template>
@@ -8,10 +11,13 @@
 import func from './func.mx'
 import { Component } from 'vue-property-decorator'
 import dUpload from '../components/d-upload/index.vue'
+import { Tooltip, Icon } from 'view-design'
 
 @Component({
 	components: {
+		'i-icon': Icon,
 		dUpload,
+		Tooltip,
 	},
 })
 export default class FuncBackground extends func {
--- src/components/d-upload/index.vue
+++ src/components/d-upload/index.vue
@@ -1,41 +1,42 @@
 <template lang="pug">
 .d-upload
-	.d-upload-img.pos-r(v-if="value")
-		img(:src="value", v-if="type === 'img'")
-		video(:src="value", v-if="type === 'video'", loop, autoplay)
+	.d-upload-img.pos-r
+		img(:src="value", v-if="value && type === 'img'")
+		video(:src="value", v-if="value && type === 'video'", loop, autoplay)
+		.progress(v-if="isShowProgress")
+			i-progress(:percent="percent" text-inside :stroke-width="16" status="active")
 		i-upload.pointer.pos-a(
 			:action="action",
 			:data="data",
+			:on-progress="handleProgress",
 			:show-upload-list="false",
+			:before-upload="handleBeforeUpload",
 			:on-success="handleSuccess")
+			.d-upload-text(v-if="!value && !isShowProgress") 点击上传
 		i-icon.d-upload-download-icon.pos-a.pointer(
+			v-if="value",
 			title="下载",
 			type="ios-download-outline",
 			color="#fff",
 			size="14",
 			@click="handleDown")
 		i-icon.d-upload-remove-icon.pos-a.pointer(
+			v-if="value",
 			title="删除",
 			type="ios-trash-outline",
 			color="#fff",
 			size="14",
 			@click="handleRemove")
-	i-upload.pointer(
-		v-else,
-		:action="action",
-		:data="data",
-		:show-upload-list="false",
-		:on-success="handleSuccess")
-		.d-upload-text 点击上传
 </template>
 <script lang="ts">
 import { baseURL } from '../../../src/api/request.js'
 import { Component, Vue, Prop } from 'vue-property-decorator'
-import { Upload, Icon } from 'view-design'
+import { Upload, Icon, Progress } from 'view-design'
 @Component({
 	components: {
 		'i-upload': Upload,
 		'i-icon': Icon,
+		'i-progress': Progress,
 	},
 })
 export default class DUpload extends Vue {
@@ -43,6 +44,9 @@ export default class DUpload extends Vue {
 	@Prop() data
 	@Prop({ default: 'img' }) type
 	@Prop() value
+
+	percent = 0
+	isShowProgress = false
 
 	handleDown() {
 		const a = document.createElement('a')
@@ -57,8 +61,27 @@ export default class DUpload extends Vue {
 	}
 
 	handleSuccess(res) {
+		this.isShowProgress = false
 		this.$emit('input', res.result.url)
 		this.$emit('success', res)
+	}
+	
+	handleProgress (event, file, fileList) {
+		console.log(event, file, fileList)
+		this.percent = event.percent
+	}
+
+	handleBeforeUpload (file) {
+		if (this.type === 'img') {
+			if (file.size > 1024 * 1024) {
+				this.$Message.error('图片大小不能超过1M')
+				return false
+			} else if (['image/jpg', 'image/png', 'image/gif'].includes(file.type)) {
+				this.$Message.error('图片仅支持jpg，png，gif')
+			}
+		}
+		this.isShowProgress = true
+		return true
 	}
 }
 </script>
@@ -113,5 +136,14 @@ export default class DUpload extends Vue {
 			}
 		}
 	}
+}
+
+.progress {
+	width: 80%;
+	position: absolute;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+	line-height: 1;
 }
 </style>
