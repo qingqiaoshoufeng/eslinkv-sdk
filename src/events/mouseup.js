22d549aecc9c0aaabe66524c1a524b0f9db36fdc
xjm <jiaming.xu@eslink.com>
2021-05-06 09:25:12 +0800

    fix: 框选方向

--- src/events/mouseup.js
+++ src/events/mouseup.js
@@ -19,14 +19,22 @@ const mouseup = (e) => {
 		const startPointerY = ruler.actions.getActualPointerY(event.state.startY)
 		const endPointerX = ruler.actions.getActualPointerX(e.clientX)
 		const endPointerY = ruler.actions.getActualPointerY(e.clientY)
+		const minPointerX = Math.min(startPointerX, endPointerX)
+		const minPointerY = Math.min(startPointerY, endPointerY)
+		const maxPointerX = Math.max(startPointerX, endPointerX)
+		const maxPointerY = Math.max(startPointerY, endPointerY)
 		Object.values(platform.state.widgetAdded).forEach(v => {
 			// 只能框选当前场景下的组件
 			if (v.scene === scene.state.index) {
+				const widgetStartX = v.config.layout.position.left
+				const widgetStartY = v.config.layout.position.top
+				const widgetEndX = v.config.layout.position.left + v.config.layout.size.width
+				const widgetEndY = v.config.layout.position.top + v.config.layout.size.height
 				if (
-					v.config.layout.position.left >= startPointerX
-					&& v.config.layout.position.top >= startPointerY
-					&& v.config.layout.position.left + v.config.layout.size.width <= endPointerX
-					&& v.config.layout.position.top + v.config.layout.size.height <= endPointerY
+					minPointerX < widgetStartX && widgetStartX < maxPointerX
+					&& minPointerY < widgetStartY && widgetStartY < maxPointerY
+					&& minPointerX < widgetEndX && widgetEndX < maxPointerX
+					&& minPointerY < widgetEndY && widgetEndY < maxPointerY
 				) {
 					platform.state.chooseWidgetId = v.id
 					platform.state.chooseWidgetState = false
