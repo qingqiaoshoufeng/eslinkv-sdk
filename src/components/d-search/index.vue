dc8bb8cfd3b423d57d5b48b04bb8e179759fc4f2
xjm <jiaming.xu@eslink.com>
2021-05-18 15:23:35 +0800

    feat: 搜索组件 #76

--- src/components/d-search/index.vue
+++ src/components/d-search/index.vue
@@ -1,43 +1,138 @@
 <template lang="pug">
-.d-detail-search.pos-a.z-index-999(
-	:class="{ active: platform.searchModal }",
-	@click="platform.searchModal = true")
-	i-input.d-detail-input(suffix="ios-search", placeholder="请输入组件名")
+.container(:class="{ active: platform.searchModal }")
+	.d-detail-search.animated.searchFadeInDown(
+		v-click-outside="close")
+		i-input.d-detail-input(suffix="ios-search", placeholder="请输入组件名" v-model="keyword")
+		ul.result
+			li(v-for="(k, i) in searchResult", :key="i" @click="check(k)") {{ k.config.widget.name }}
 </template>
 <script lang="ts">
 import platform from '../../store/platform.store'
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Watch } from 'vue-property-decorator'
 import { Icon, Input } from 'view-design'
+import ClickOutside from 'vue-click-outside'
+import scene from '../../store/scene.store'
 
 @Component({
 	components: {
 		'i-icon': Icon,
 		'i-input': Input,
 	},
+	directives: { ClickOutside },
 })
 export default class DSearch extends Vue {
+	searchResult = []
 	platform = platform.state
+	scene = scene.state
+	keyword = ''
+	
+	@Watch('keyword')
+	keywordChange (val) {
+		if (!val) {
+			this.searchResult = []
+			return
+		}
+		let arr = []
+		for (const key in this.platform.widgetAdded) {
+			if (this.platform.widgetAdded[key].config.widget.name.includes(val)) {
+				arr.push(this.platform.widgetAdded[key])
+			}
+		}
+		this.searchResult = arr
+	}
+
+	close() {
+		if (this.platform.searchModal) {
+			this.platform.searchModal = false
+		}
+	}
+
+	check(widget) {
+		this.scene.index = widget.scene
+		this.platform.chooseWidgetId = widget.id
+		this.platform.searchModal = false
+	}
 }
 </script>
 <style lang="scss" scoped>
+.container {
+	display: none;
+	position: fixed;
+	left: 0;
+	right: 0;
+	top: 0;
+	bottom: 0;
+	z-index: 9999;
+	&.active {
+		display: block;
+	}
+}
 .d-detail-search {
-	opacity: 0;
-	top: -200px;
-	width: 70%;
+	position: absolute;
+	top: 100px;
 	left: 50%;
+	width: 70%;
 	margin-left: calc(-35% + 18px);
-	transition: all 0.3s;
 	border-radius: 4px;
+
 	::v-deep {
 		.d-detail-input {
 			input {
-				background-color: transparent;
+				background: rgba(10, 10, 14, 0.7);
+				color: #fff;
 			}
 		}
 	}
-	&.active {
+}
+.result {
+	margin-top: 10px;
+	max-height: 200px;
+	overflow-y: auto;
+	background: rgba(10, 10, 14, 0.7);
+	li {
+		height: 30px;
+		line-height: 30px;
+		margin: 4px 0;
+		padding: 0 10px;
+		color: #fff;
+		&:hover {
+			background: #2d8cf0;
+		}
+	}
+	&::-webkit-scrollbar {
+		width: 4px;
+		height: 1px;
+		transition: all 0.3s;
+	}
+
+	&::-webkit-scrollbar-thumb {
+		background: rgba(83, 83, 83, 1);
+		border-radius: 10px;
+		transition: all 0.3s;
+	}
+
+	&::-webkit-scrollbar-track {
+		background: rgba(237, 237, 237, 1);
+		border-radius: 10px;
+		transition: all 0.3s;
+	}
+}
+
+.searchFadeInDown {
+	animation-name: searchFadeInDown;
+}
+
+@keyframes searchFadeInDown {
+	0% {
+		opacity: 0;
+		-webkit-transform: translateY(-20px);
+		transform: translateY(-20px);
+	}
+
+	100% {
 		opacity: 1;
-		top: 100px;
+		-webkit-transform: translateY(0);
+		transform: translateY(0);
 	}
 }
 </style>
