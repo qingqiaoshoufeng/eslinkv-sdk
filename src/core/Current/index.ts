f94153d52720ce05d92e1c92c7f7e3b161b38cc5
zhepei.shen <zhepei.shen@eslink.com>
2021-06-10 20:48:18 +0800

    feat: current

--- src/core/Current/index.ts
+++ src/core/Current/index.ts
@@ -31,7 +31,7 @@ export default class Current extends Factory<Current> {
 	/* 当前场景 */
 	currentSceneIndex: number | string = 0
 	/* 当前打开的场景集合 */
-	currentCreateSceneList: Array<number|string> = [0]
+	currentCreateSceneList: Array<number | string> = [0]
 	/* 当前选中组件-多组件配置 */
 	currentWidgetListConfig = {
 		left: 0,
@@ -107,7 +107,6 @@ export default class Current extends Factory<Current> {
 		if (val === undefined || val === null) return
 		this.currentSceneIndex = val
 		this.activeSceneId = val
-		// this.currentCreateSceneList = [val]
 		let event = new CustomEvent('SceneIndex', { detail: { index: val } })
 		document.dispatchEvent(event)
 		event = null
@@ -120,7 +119,6 @@ export default class Current extends Factory<Current> {
 	/* 关闭场景 */
 	closeScene(id: string): void {
 		const index = this.currentCreateSceneList.findIndex(v => v === id)
-		console.info(index)
 		this.currentCreateSceneList.splice(index, 1)
 		this.currentCreateSceneList = [...this.currentCreateSceneList]
 		let event = new CustomEvent('DestroyScene', { detail: { index } })
