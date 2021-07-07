3495b5bc2944f13b9cc5561394fa8d4202b48e8e
fish-uncle <fishuncle@126.com>
2021-07-07 09:45:38 +0800

    feat: bug

--- src/vue2/components-func/func-input.vue
+++ src/vue2/components-func/func-input.vue
@@ -1,9 +1,6 @@
 <template lang="pug">
 d-right-control(:label="`${config.label} [${inputKey}]`")
-	i-input(
-		v-model="obj[inputKey]",
-		@on-focus="event.inputFocus = true",
-		@on-blur="event.inputFocus = false")
+	i-input(v-model="obj[inputKey]")
 </template>
 <script lang="ts">
 import func from './func.mx'
